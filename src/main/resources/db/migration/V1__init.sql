3d6d09dd51affd6da804dd4e71b21289bec78364
Kim MinJun <104950862+nuj1min@users.noreply.github.com>
2026-07-08 11:16:56 +0900

    Update V1__init.sql

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -18,7 +18,7 @@ CREATE TABLE `user` (
     id BIGINT NOT NULL AUTO_INCREMENT,
     nickname VARCHAR(50) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NULL,
     gender VARCHAR(10) NOT NULL,
     birth_year VARCHAR(4) NOT NULL,
     region_id BIGINT NOT NULL,
