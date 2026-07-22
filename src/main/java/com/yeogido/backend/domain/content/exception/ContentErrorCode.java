6329ff99f3ace8cd012bd4d6a89f864eeeebe52c
Seoyeon-park0103 <seoyeon-park@naver.com>
2026-07-22 15:50:50 +0900

    chore: 불필요한 미사용 에러 코드 제거

--- src/main/java/com/yeogido/backend/domain/content/exception/ContentErrorCode.java
+++ src/main/java/com/yeogido/backend/domain/content/exception/ContentErrorCode.java
@@ -13,25 +13,9 @@ public enum ContentErrorCode implements ErrorCode {
             HttpStatus.NOT_FOUND,
             "CONTENT4041",
             "콘텐츠가 존재하지 않습니다."
-    ),
-
-    CONTENT_LIKE_NOT_FOUND(
-            HttpStatus.NOT_FOUND,
-            "CONTENT_LIKE_NOT_FOUND",
-            "좋아요 정보를 찾을 수 없습니다."
-    ),
-
-    CONTENT_ALREADY_DELETE(
-            HttpStatus.CONFLICT,
-            "CONTENT4091",
-                    "이미 삭제된 문화콘텐츠입니다."
-    ),
-
-    CONTENT_ALREADY_LIKED(
-            HttpStatus.CONFLICT,
-            "CONTENT4092",
-            "이미 좋아요를 누른 문화콘텐츠입니다."
     );
+
+
     private final HttpStatus httpStatus;
     private final String code;
     private final String message;
