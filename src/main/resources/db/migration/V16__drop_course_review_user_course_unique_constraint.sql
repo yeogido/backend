f50a85cdf9ecbb4e81f145c6ef65014b63d939ef
yangjiae12 <yangjiae12@gmail.com>
2026-07-25 22:50:55 +0900

    fix: Flyway V16 마이그레이션 수정 #127

--- src/main/resources/db/migration/V16__drop_course_review_user_course_unique_constraint.sql
+++ src/main/resources/db/migration/V16__drop_course_review_user_course_unique_constraint.sql
@@ -1,2 +1,5 @@
 ALTER TABLE course_review
-    DROP INDEX uk_course_review_user_course;
+    ADD INDEX idx_course_review_user_id (user_id);
+
+ALTER TABLE course_review
+DROP INDEX uk_course_review_user_course;
